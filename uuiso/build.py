302945e1e85f921bacc2a6555d5ea31c0ef28d67
Mate Lakat <mlakat@mlakat.hu>
2014-05-16 21:14:52 +0100

    Refactor - create iso module
    ISO file related operations moved to a separate module.

--- uuiso/build.py
+++ uuiso/build.py
@@ -1,90 +1,7 @@
 import argparse
 import textwrap
 
-
-class IsoMounter(object):
-    def __init__(self, path, file_checker=None, executor=None, tmpmaker=None,
-                 binary_checker=None):
-        self.path = path
-        self.file_checker = file_checker
-        self.executor = executor
-        self.tmpmaker = tmpmaker
-        self.binary_checker = binary_checker
-        self.iso_mountpoint = None
-        self.merged_dir = None
-        self.overlay_dir = None
-
-    def validate(self):
-        if self.file_checker(self.path):
-            if self.binary_checker('fuseiso'):
-                if self.binary_checker('unionfs-fuse'):
-                    return True
-        return False
-
-    def mount(self):
-        self.iso_mountpoint = self.tmpmaker()
-        self.overlay_dir = self.tmpmaker()
-        self.merged_dir = self.tmpmaker()
-        self.executor(
-            ['fuseiso', self.path, self.iso_mountpoint])
-        self.executor(
-            [
-                'unionfs-fuse',
-                '-o',
-                'cow',
-                ':'.join(
-                    [self.overlay_dir + '=RW', self.iso_mountpoint + '=RO']),
-                self.merged_dir
-            ]
-        )
-
-    def umount(self):
-        self.executor(
-            ['fusermount', '-u', self.merged_dir])
-        self.executor(
-            ['fusermount', '-u', self.iso_mountpoint])
-
-    def make_file_writable(self, fpath):
-        import os
-        import stat
-        import shutil
-
-        overlay_path = os.path.join(self.overlay_dir, fpath)
-        iso_path = os.path.join(self.iso_mountpoint, fpath)
-
-        overlay_dir = os.path.dirname(overlay_path)
-        if not os.path.exists(overlay_dir):
-            os.makedirs(overlay_dir)
-        shutil.copy(iso_path, overlay_path)
-        os.chmod(overlay_path, os.stat(overlay_path).st_mode | stat.S_IWUSR)
-        return overlay_path
-
-
-
-class IsoCreator(object):
-    def __init__(self, source_dir, target_file, executor=None):
-        self.source_dir = source_dir
-        self.target_file = target_file
-        self.executor = executor
-
-    def create(self):
-        self.executor([
-            'mkisofs',
-            '-r',
-            '-V',
-            'Automated Ubuntu Install CD',
-            '-cache-inodes',
-            '-J',
-            '-l',
-            '-b', 'isolinux/isolinux.bin',
-            '-c', 'isolinux/boot.cat',
-            '-no-emul-boot',
-            '-boot-load-size', '4',
-            '-boot-info-table',
-            '-quiet',
-            '-o', self.target_file,
-            self.source_dir,
-            ])
+from uuiso import iso
 
 
 class TmpMaker(object):
@@ -638,8 +555,10 @@ def main():
 
     tmp_maker = TmpMaker(tempfile.mkdtemp)
 
-    mounter = IsoMounter(
+    mounter = iso.IsoMounter(
         options.official, os.path.exists, subprocess.call, tmp_maker)
+
+    mounter.validate()
 
     mounter.mount()
 
@@ -670,7 +589,7 @@ def main():
         with open(os.path.join(mounter.overlay_dir, 'after_install.sh'), 'wb') as after_install:
             after_install.write(after_install_script_contents)
 
-        iso_maker = IsoCreator(
+        iso_maker = iso.IsoCreator(
             mounter.merged_dir, options.automated, executor=subprocess.call)
 
         iso_maker.create()
---
+++ uuiso/test/test_iso.py
@@ -0,0 +1,140 @@
+import unittest
+import mock
+
+from uuiso import iso
+
+
+class IsoCreator(unittest.TestCase):
+    def test_create(self):
+        creator = iso.IsoCreator('somedir', 'target.iso')
+        mock_executor = mock.Mock()
+        creator.executor = mock_executor
+
+        creator.create()
+
+        self.assertEquals([
+            mock.call([
+                'mkisofs',
+                '-r',
+                '-V',
+                'Automated Ubuntu Install CD',
+                '-cache-inodes',
+                '-J',
+                '-l',
+                '-b', 'isolinux/isolinux.bin',
+                '-c', 'isolinux/boot.cat',
+                '-no-emul-boot',
+                '-boot-load-size', '4',
+                '-boot-info-table',
+                '-quiet',
+                '-o', 'target.iso',
+                'somedir'
+            ])],
+            mock_executor.mock_calls)
+
+
+def tempdirmaker():
+    counter = []
+
+    def tmpmaker():
+        result = 'tempdir%s' % len(counter)
+        counter.append('l')
+        return result
+
+    return tmpmaker
+
+
+def exists(fname):
+    return True
+
+
+def missing(fname):
+    return False
+
+
+class TestIsoMounter(unittest.TestCase):
+    def test_validate_file_exists(self):
+        mounter = iso.IsoMounter('isofile', file_checker=exists,
+                                   binary_checker=exists)
+
+        self.assertTrue(mounter.validate())
+
+    def test_validate_binary_missing(self):
+        mounter = iso.IsoMounter('isofile', file_checker=exists,
+                                   binary_checker=missing)
+
+        self.assertFalse(mounter.validate())
+
+    def test_validate_checks_for_all_binaries(self):
+        mock_checker = mock.Mock()
+        mock_checker.return_value = True
+        mounter = iso.IsoMounter('isofile', file_checker=exists,
+                                   binary_checker=mock_checker)
+
+        mounter.validate()
+
+        self.assertEquals(
+            [
+                mock.call('fuseiso'),
+                mock.call('unionfs-fuse'),
+            ], mock_checker.mock_calls)
+
+    def test_validate_file_missing(self):
+        mounter = iso.IsoMounter('isofile', file_checker=missing)
+
+        self.assertFalse(mounter.validate())
+
+    def test_mount_succeeds(self):
+        mounter = iso.IsoMounter(
+            'isofile', executor=mock.Mock(), tmpmaker=tempdirmaker())
+
+        mounter.mount()
+
+        self.assertEquals(
+            [
+                mock.call(['fuseiso', 'isofile', 'tempdir0']),
+                mock.call([
+                    'unionfs-fuse', '-o', 'cow',
+                    'tempdir1=RW:tempdir0=RO', 'tempdir2']),
+            ], mounter.executor.mock_calls)
+
+    def test_mount_sets_iso_mountpoint(self):
+        mounter = iso.IsoMounter(
+            'isofile', executor=mock.Mock(), tmpmaker=tempdirmaker())
+
+        mounter.mount()
+
+        self.assertEquals('tempdir0', mounter.iso_mountpoint)
+
+    def test_mount_sets_overlay_dir(self):
+        mounter = iso.IsoMounter(
+            'isofile', executor=mock.Mock(), tmpmaker=tempdirmaker())
+
+        mounter.mount()
+
+        self.assertEquals('tempdir1', mounter.overlay_dir)
+
+    def test_mount_sets_merged_dir(self):
+        mounter = iso.IsoMounter(
+            'isofile', executor=mock.Mock(), tmpmaker=tempdirmaker())
+
+        mounter.mount()
+
+        self.assertEquals('tempdir2', mounter.merged_dir)
+
+    def test_umount(self):
+        mounter = iso.IsoMounter(
+            'isofile', executor=mock.Mock(), tmpmaker=tempdirmaker())
+        mounter.mount()
+        mounter.executor = mock.Mock()
+
+        mounter.umount()
+
+        self.assertEquals(
+            [
+                mock.call(['fusermount', '-u', 'tempdir2']),
+                mock.call(['fusermount', '-u', 'tempdir0']),
+            ], mounter.executor.mock_calls)
+
+
+
