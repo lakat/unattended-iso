a3bd1248c77e4f306ab535dfe9241e56a97fe549
Mate Lakat <mlakat@mlakat.hu>
2014-05-31 15:47:20 +0100

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='uiso',
-      version='0.2.0-dev',
+      version='0.2.0',
       packages=find_packages(),
       package_data = {
           'uiso': [
