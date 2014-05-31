dc1c161bbc46fac0cce0a52377f076ca09e0d4bf
Mate Lakat <mlakat@mlakat.hu>
2014-05-31 15:41:44 +0100

    Add package_data

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
 
+
 setup(name='uiso',
       version='0.2.0-dev',
       packages=find_packages(),
+      package_data = {
+          'uiso': [
+            'centos/isolinux.cfg.5',
+            'centos/isolinux.cfg.6',
+            'centos/ks.cfg.5',
+            'centos/ks.cfg.6',
+            'centos/post_install.sh',
+            'ubuntu/autoinst.seed',
+            'ubuntu/post_install.sh',
+            'ubuntu/txt.cfg',
+            ]
+      },
       entry_points={
           'console_scripts': [
               'uiso-ubuntu = uiso.ubuntu.build:main',
