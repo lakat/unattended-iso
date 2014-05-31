a7f9583eb45a2d186bf9d3bd680ea48774a8f526
Mate Lakat <mlakat@mlakat.hu>
2014-05-31 10:14:17 +0100

    Bump revision number for next release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='uiso',
-      version='0.1.0',
+      version='0.2.0-dev',
       packages=['uiso'],
       entry_points={
           'console_scripts': [
